cdfdc741b682807667a5f508bfca862464f00ba2
6124437 <arjanvanhoogdalem@gmail.com>
2023-03-17 20:37:33 +0100

    initial commit

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { RequestTokensDto } from 'dtos/RequestTokensDto';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/token-contract-address')
+  getTokenContractAddress(): { address: string } {
+    return { address: this.appService.getTokenContractAddress() };
+  }
+
+  @Get('/lottery-contract-address')
+  getLotteryContractAddress(): { address: string } {
+    return { address: this.appService.getLotteryContractAddress() };
+  }
+
+  @Get('/transaction-status')
+  async getTransactionStatus(@Query('hash') hash: string): Promise<string> {
+    return this.appService.getTransactionStatus(hash);
+  }
+
+  @Post('/request-tokens')
+  async requestTokens(@Body() body: RequestTokensDto) {
+    return { result: this.appService.requestTokens(body.address, body.amount) };
+  }
+}
