e9a97cfd96830eb9c01d46fd1ea12979032eb6ef
noam <36198454+noamrubin22@users.noreply.github.com>
2023-03-18 23:00:49 +0100

    rm private-keys

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   privateKey:
-    '07e1d800663af9624a76fd384a19d8c3df9465e2cf3df3c902aca6a11950842b',
-  alchemyApiKey: 'agBsT3lo85Nb3h6crO2-_k9MxEPBqbzJ',
+    '',
+  alchemyApiKey: '',
 };
